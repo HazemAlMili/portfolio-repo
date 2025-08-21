e36a548c1d7cb424cae86f9b245f72660b316e09
HazemAlMelli <hazemalmile7@gmail.com>
2025-08-21 22:32:34 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export const metadata: Metadata = {
     title: "Hazem Al-Melli | Front-End Developer",
     description:
       "Portfolio showcasing modern, responsive web projects built with React & Next.js.",
-    url: "https://YOUR-DOMAIN.com",
+    url: "https://portfolio-repo-pearl.vercel.app/",
     siteName: "Hazem Al-Melli Portfolio",
     images: [
       {
-        url: "https://YOUR-DOMAIN.com/images/preview.png",
+        url: "https://portfolio-repo-pearl.vercel.app/images/Hero.png",
         width: 1200,
         height: 630,
         alt: "Hazem Al-Melli Portfolio Preview",
@@ -43,7 +43,10 @@ export const metadata: Metadata = {
     title: "Hazem Al-Melli | Front-End Developer",
     description:
       "Portfolio showcasing modern, responsive web projects built with React & Next.js.",
-    images: ["https://YOUR-DOMAIN.com/images/preview.png"],
+    images: ["https://portfolio-repo-pearl.vercel.app/images/Hero.png"],
+  },
+  icons: {
+    icon: ["https://portfolio-repo-pearl.vercel.app/images/logo.svg"],
   },
 };
 
@@ -61,6 +64,7 @@ export default function RootLayout({
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+        <meta name="author" content="[Hazem Al-Melli]"></meta>
         <meta
           property="og:title"
           content="Hazem Al-Melli | Front-End Developer Portfolio"
@@ -71,9 +75,12 @@ export default function RootLayout({
         />
         <meta
           property="og:image"
-          content="https://your-domain.com/images/preview.png"
+          content="https://portfolio-repo-pearl.vercel.app/images/Hero.png"
         />
-        <meta property="og:url" content="https://your-domain.com" />
+        <meta
+          property="og:url"
+          content="https://portfolio-repo-pearl.vercel.app/"
+        />
         <meta name="twitter:card" content="summary_large_image" />
       </head>
       <body className="font-serif antialiased">{children}</body>
