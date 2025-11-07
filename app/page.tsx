bc008a8d5ae3de23c4bc09af6ebd670efdcd63ec
HazemAlMelli <hazemalmile7@gmail.com>
2025-11-07 23:50:09 +0200

    enhance

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,29 @@ import Projects from "@/components/projects";
 import Certificates from "@/components/certificates";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
+import ClickSpark from "@/components/ClickSpark";
+
 // import Message from "@/components/dev-mess";
 import "./globals.css";
 function Home() {
   return (
     <main>
-      <Header />
-      <Hero />
-      <About />
-      <Projects />
-      <Certificates />
-      <Contact />
-      <Footer />
-      {/* <Message /> */}
+      <ClickSpark
+        sparkColor="#fff"
+        sparkSize={13}
+        sparkRadius={12}
+        sparkCount={7}
+        duration={400}
+      >
+        <Header />
+        <Hero />
+        <About />
+        <Projects />
+        <Certificates />
+        <Contact />
+        <Footer />
+        {/* <Message /> */}
+      </ClickSpark>
     </main>
   );
 }
